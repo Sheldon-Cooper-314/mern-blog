00ce3f313526095123add745f2734d5e03531f0b
shishirallu <shishir.211ec101@nitk.edu.in>
2024-07-04 17:50:16 +0530

    added comment full functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { test,updateUser, deleteUser, signout } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { getUsers } from '../controllers/user.controller.js';
+import { getUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout',signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
