0e8b3eb801a024030a81c3ae66c3b7d643efff98
shishirallu <shishir.211ec101@nitk.edu.in>
2024-07-04 15:23:12 +0530

    create get user added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import FooterCom from "./components/FooterCom"
 import PrivateRoute from "./components/PrivateRoute"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost"
+import UpdatePost from "./components/UpdatePost"
 
 
 export default function App() {
@@ -26,6 +27,7 @@ export default function App() {
       </Route>
       <Route element={<OnlyAdminPrivateRoute/>} >
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/update-post/:postId" element={<UpdatePost />} />
       </Route>
       
       <Route path="/projects" element={<Projects />} />
